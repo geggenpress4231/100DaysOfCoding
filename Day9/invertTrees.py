ec0192fe7a83b90ab20d434c5dd1cf79d63c8e9a
geggenpress4231 <ami.utsarabbani@gmail.com>
2024-05-30 22:47:50 -0400

    tree problem 2 fixed

---
+++ Day9/invertTrees.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return None
+        
+        # Swap the left and right children
+        root.left, root.right = root.right, root.left
+        
+        # Recursively invert the left and right subtrees
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root
