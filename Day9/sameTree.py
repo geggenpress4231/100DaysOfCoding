f2d85167f84cebc89e82006c52da4dc0ecb58860
geggenpress4231 <ami.utsarabbani@gmail.com>
2024-06-01 21:31:33 -0400

    fixed tree problem

--- Day9/sameTree.py
+++ Day9/sameTree.py
@@ -1,13 +1,13 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # If both trees are empty, they are the same
+       
         if not p and not q:
             return True
-        # If one of the trees is empty, they are not the same
+     
         if not p or not q:
             return False
-        # If the values of the current nodes are different, they are not the same
+      
         if p.val != q.val:
             return False
-        # Recursively check the left and right subtrees
+        
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
