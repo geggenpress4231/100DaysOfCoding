77feafd3ca7b59c8ed17f3cbbaee7cf7bb708dcd
geggenpress4231 <ami.utsarabbani@gmail.com>
2024-08-16 22:26:02 -0400

    nonoverlappingintervals

--- nonOverlappingIntervals.py
+++ nonOverlappingIntervals.py
@@ -6,12 +6,14 @@ class Solution:
         intervals.sort(key=lambda x: x[1])
         
         removeInterval = 0
-        lastEnd = intervals[0][1]
+        upperLimit = intervals[0][1]
+        lowerLimit = intervals[0][0]
         
         for i in range(1, len(intervals)):
-            if intervals[i][0] < lastEnd:
+            if intervals[i][0] < upperLimit:
                 removeInterval += 1
             else:
-                lastEnd = intervals[i][1]
+                upperLimit = intervals[i][1]
+                lowerLimit = intervals[i][0]
         
         return removeInterval
