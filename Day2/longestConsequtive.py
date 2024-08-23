d2616ea9391fce9c0c56c9e00161d48e88faa603
geggenpress4231 <ami.utsarabbani@gmail.com>
2024-08-22 21:53:22 -0400

    alternate solution using set

--- Day2/longestConsequtive.py
+++ Day2/longestConsequtive.py
@@ -20,4 +20,17 @@ class Solution:
 
         max_len = max(max_len, current_len)  
         return max_len
+#aliter better
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
 
+        for n in numSet:
+            # check if its the start of a sequence
+            if (n - 1) not in numSet:
+                length = 1
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
