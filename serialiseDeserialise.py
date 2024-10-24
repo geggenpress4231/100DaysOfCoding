ad644488033be1ed25b26dd54d0bd16da3fbac69
geggenpress4231 <ami.utsarabbani@gmail.com>
2024-10-24 14:35:28 -0400

    invert binary tree

---
+++ serialiseDeserialise.py
@@ -0,0 +1,30 @@
+class Codec:
+
+    def serialize(self, root: TreeNode) -> str:
+        res = []
+        
+        def dfs(node):
+            if not node:
+                res.append("N")
+                return
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        
+        dfs(root)
+        return ",".join(res)
+    
+    def deserialize(self, data: str) -> TreeNode:
+        vals = iter(data.split(","))
+        
+        def dfs():
+            val = next(vals)
+            if val == "N":
+                return None
+            node = TreeNode(int(val))
+            node.left = dfs()
+            node.right = dfs()
+            return node
+        
+        return dfs()
+#aliter
